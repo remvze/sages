50187641a64ab20093b71f14e7e9a4c1a34e52a6
MAZE <remvze@proton.me>
2022-10-21 01:31:35 -0700

    feat: add base data

---
+++ src/data/authors.js
@@ -0,0 +1,16 @@
+import { priority, sex } from './const.js';
+
+const authors = [
+  {
+    name: 'Albert Camus',
+    priority: priority.high,
+    sex: sex.male,
+  },
+  {
+    name: 'Virginia Woolf',
+    priority: priority.high,
+    sex: sex.female,
+  },
+];
+
+export default authors;
---
+++ src/data/const.js
@@ -0,0 +1,10 @@
+export const priority = {
+  low: 1,
+  medium: 2,
+  high: 3,
+};
+
+export const sex = {
+  male: 'male',
+  female: 'female',
+};
