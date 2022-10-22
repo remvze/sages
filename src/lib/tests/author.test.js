bef1bf2f3666d298cf14c2329607193d7e12255f
MAZE <remvze@proton.me>
2022-10-22 06:17:11 -0700

    test: write tests for priority filter

--- src/lib/tests/author.test.js
+++ src/lib/tests/author.test.js
@@ -48,10 +48,41 @@ describe('Select a random author', () => {
   it('Should return author according to the given sex', () => {
     const male = selectRandomAuthor(authors, history, { sex: 'male' });
     const female = selectRandomAuthor(authors, history, { sex: 'female' });
+
     const maleObject = authors.find(author => author.name === male);
     const femaleObject = authors.find(author => author.name === female);
 
     expect(maleObject.sex).toBe('male');
     expect(femaleObject.sex).toBe('female');
+  });
+
+  it('Should return author according to the given priority', () => {
+    const low = selectRandomAuthor(authors, history, { priority: 1 });
+    const medium = selectRandomAuthor(authors, history, { priority: 2 });
+    const high = selectRandomAuthor(authors, history, { priority: 3 });
+
+    const lowObject = authors.find(author => author.name === low);
+    const highObject = authors.find(author => author.name === high);
+
+    expect(lowObject.priority).toBe(1);
+    expect(medium).toBeNull();
+    expect(highObject.priority).toBe(3);
+  });
+
+  it('Should filter based on both sex and priority', () => {
+    const author1 = selectRandomAuthor(authors, history, {
+      priority: 1,
+      sex: 'female',
+    });
+    const author1Object = authors.find(author => author.name === author1);
+
+    const author2 = selectRandomAuthor(authors, history, {
+      priority: 3,
+      sex: 'female',
+    });
+
+    expect(author1Object.sex).toBe('female');
+    expect(author1Object.priority).toBe(1);
+    expect(author2).toBeNull();
   });
 });
