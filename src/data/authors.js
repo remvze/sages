cb77c59b519d6dff0c3fa36bc0ccc76890271515
MAZE <remvze@proton.me>
2022-10-23 00:37:15 -0700

    feat: add authors

--- src/data/authors.js
+++ src/data/authors.js
@@ -8,15 +8,37 @@ import { priority, sex } from './const.js';
  * }>}
  */
 const authors = [
+  /**
+   * High Priority
+   */
   {
     name: 'Albert Camus',
     priority: priority.high,
     sex: sex.male,
   },
   {
-    name: 'Virginia Woolf',
+    name: 'Haruki Murakami',
     priority: priority.high,
-    sex: sex.female,
+    sex: sex.male,
+  },
+  {
+    name: 'Bertrand Russell',
+    priority: priority.high,
+    sex: sex.male,
+  },
+  {
+    name: 'Erich Fromm',
+    priority: priority.high,
+    sex: sex.male,
+  },
+
+  /**
+   * Low Priority
+   */
+  {
+    name: 'Fyodor Dostoevsky',
+    priority: priority.low,
+    sex: sex.male,
   },
   {
     name: 'Aldous Huxley',
@@ -24,7 +46,37 @@ const authors = [
     sex: sex.male,
   },
   {
-    name: 'Viktor Frankl',
+    name: 'Voltaire',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Sylvia Plath',
+    priority: priority.low,
+    sex: sex.female,
+  },
+  {
+    name: 'Plato',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Virginia Woolf',
+    priority: priority.low,
+    sex: sex.female,
+  },
+  {
+    name: 'Charles Bukowski',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Carl Jung',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Leo Tolstoy',
     priority: priority.low,
     sex: sex.male,
   },
@@ -34,7 +86,67 @@ const authors = [
     sex: sex.male,
   },
   {
-    name: 'Sylvia Plath',
+    name: 'Arthur Schopenhauer',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'George Orwell',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Socrates',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Anton Chekhov',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Søren Kierkegaard',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Franz Kafka',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'C. S. Lewis',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Jean-Paul Sartre',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Seneca',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Epictetus',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Aristotle',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Jacques Derrida',
+    priority: priority.low,
+    sex: sex.male,
+  },
+  {
+    name: 'Oscar Wilde',
     priority: priority.low,
     sex: sex.male,
   },
