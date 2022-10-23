b7c2ac067c34fe8e9bcc63ba605e041410464bea
MAZE <remvze@proton.me>
2022-10-23 05:32:52 -0700

    feat: add male and female count

--- src/scripts/count.js
+++ src/scripts/count.js
@@ -1,6 +1,13 @@
 import authors from '../data/authors.js';
+import { sex } from '../data/const.js';
 import logger from '../lib/logger.js';
 
-const count = authors.length;
+const allCount = authors.length;
+const { length: maleCount } = authors.filter(author => author.sex === sex.male);
+const { length: femaleCount } = authors.filter(
+  author => author.sex === sex.female
+);
 
-logger.success(`${count} authors are on the list.`);
+logger.success(
+  `${allCount} authors are on the list. (${femaleCount} female, ${maleCount} male)`
+);
